2afe90fc1fd1f75a6d01f0568262c0ee3d502f28
Christopher Groskopf <staringmonkey@gmail.com>
2014-04-25 15:15:14 -0500

    Rev to 0.2.0 for dev.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='journalism',
-    version='0.1.0',
+    version='0.2.0',
     description='',
     long_description=open('README').read(),
     author='Christopher Groskopf',
