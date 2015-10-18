a6f507797bf257eb86aeabfad038df595705328d
Christopher Groskopf <staringmonkey@gmail.com>
2015-10-17 20:34:49 -0500

    Reorganizing column tests.

--- tests/test_columns.py
+++ tests/test_columns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-import datetime
 import pickle
 
 try:
@@ -18,7 +17,79 @@ from agate import Table
 from agate.data_types import *
 from agate.exceptions import ColumnDoesNotExistError
 
-class TestColumns(unittest.TestCase):
+class TestColumn(unittest.TestCase):
+    def setUp(self):
+        self.rows = (
+            (1, 2, 'a'),
+            (2, 3, 'b'),
+            (None, 4, 'c')
+        )
+
+        self.number_type = Number()
+        self.text_type = Text()
+
+        self.columns = (
+            ('one', self.number_type),
+            ('two', self.number_type),
+            ('three', self.text_type)
+        )
+
+        self.table = Table(self.rows, self.columns)
+        
+    def test_stringify(self):
+        self.assertEqual(str(self.table.columns['one']), "<agate.Column: (Decimal('1'), Decimal('2'), None)>")
+
+    def test_stringify_long(self):
+        rows = (
+            (1, 2, 'a'),
+            (2, 3, 'b'),
+            (None, 4, 'c'),
+            (1, 2, 'a'),
+            (2, 3, 'b'),
+            (None, 4, 'c')
+        )
+
+        self.table = Table(rows, self.columns)
+
+        self.assertEqual(str(self.table.columns['one']), "<agate.Column: (Decimal('1'), Decimal('2'), None, Decimal('1'), Decimal('2'), ...)>")
+
+    def test_column_length(self):
+        self.assertEqual(len(self.table.columns['one']), 3)
+
+    def test_get_column_item(self):
+        self.assertEqual(self.table.columns['one'][1], 2)
+
+    def test_column_contains(self):
+        self.assertEqual(1 in self.table.columns['one'], True)
+        self.assertEqual(3 in self.table.columns['one'], False)
+
+    def test_columns_equal(self):
+        table2 = Table(self.rows, self.columns)
+
+        self.assertTrue(self.table.columns['one'] == table2.columns['one'])
+
+    def test_columns_not_equal(self):
+        table2 = Table(self.rows, self.columns)
+
+        self.assertFalse(self.table.columns['one'] != table2.columns['one'])
+
+    def test_immutable(self):
+        with self.assertRaises(TypeError):
+            self.table.columns['one'] = 'foo'
+
+        with self.assertRaises(TypeError):
+            self.table.columns['one'][0] = 100
+
+    def test_properties(self):
+        column = self.table.columns['one']
+
+        self.assertEqual(column.name, 'one')
+        self.assertIs(column.data_type, self.number_type)
+
+    def test_pickleable(self):
+        pickle.dumps(self.table.columns['one'])
+
+class TestColumnSequence(unittest.TestCase):
     def setUp(self):
         self.rows = (
             (1, 2, 'a'),
@@ -59,22 +130,6 @@ class TestColumns(unittest.TestCase):
         with self.assertRaises(ColumnDoesNotExistError):
             self.table.columns[3]
 
-    def test_stringify(self):
-        self.assertEqual(str(self.table.columns['one']), "<agate.Column: (Decimal('1'), Decimal('2'), None)>")
-
-    def test_stringify_long(self):
-        rows = (
-            (1, 2, 'a'),
-            (2, 3, 'b'),
-            (None, 4, 'c'),
-            (1, 2, 'a'),
-            (2, 3, 'b'),
-            (None, 4, 'c')
-        )
-
-        self.table = Table(rows, self.columns)
-
-        self.assertEqual(str(self.table.columns['one']), "<agate.Column: (Decimal('1'), Decimal('2'), None, Decimal('1'), Decimal('2'), ...)>")
 
     def test_length(self):
         self.assertEqual(len(self.table.columns), 3)
@@ -97,16 +152,6 @@ class TestColumns(unittest.TestCase):
         with self.assertRaises(ColumnDoesNotExistError):
             self.table.columns['four']
 
-    def test_column_length(self):
-        self.assertEqual(len(self.table.columns['one']), 3)
-
-    def test_get_column_item(self):
-        self.assertEqual(self.table.columns['one'][1], 2)
-
-    def test_column_contains(self):
-        self.assertEqual(1 in self.table.columns['one'], True)
-        self.assertEqual(3 in self.table.columns['one'], False)
-
     def test_iterate_columns(self):
         it = iter(self.table.columns)
 
@@ -116,29 +161,3 @@ class TestColumns(unittest.TestCase):
 
         with self.assertRaises(StopIteration):
             next(it)
-
-    def test_columns_equal(self):
-        table2 = Table(self.rows, self.columns)
-
-        self.assertTrue(self.table.columns['one'] == table2.columns['one'])
-
-    def test_columns_not_equal(self):
-        table2 = Table(self.rows, self.columns)
-
-        self.assertFalse(self.table.columns['one'] != table2.columns['one'])
-
-    def test_immutable(self):
-        with self.assertRaises(TypeError):
-            self.table.columns['one'] = 'foo'
-
-        with self.assertRaises(TypeError):
-            self.table.columns['one'][0] = 100
-
-    def test_properties(self):
-        column = self.table.columns['one']
-
-        self.assertEqual(column.name, 'one')
-        self.assertIs(column.data_type, self.number_type)
-
-    def test_pickleable(self):
-        pickle.dumps(self.table.columns['one'])
