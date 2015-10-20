f71b3a0b383822b4ffb328f685de910b57f6057a
Christopher Groskopf <staringmonkey@gmail.com>
2015-10-19 23:06:43 -0500

    Break another test.

--- tests/test_tableset.py
+++ tests/test_tableset.py
@@ -126,6 +126,8 @@ class TestTableSet(unittest.TestCase):
         self.assertIsInstance(table.column_types[0], Text)
         self.assertSequenceEqual(table.column_types[1:], [self.text_type, self.number_type])
 
+        raise('TODO: Should set row_names')
+
     def test_compute(self):
         tableset = TableSet(self.tables)
 
