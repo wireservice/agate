61c0de77d0ed2c01ca87f5ad0d30f5051084dadd
Christopher Groskopf <staringmonkey@gmail.com>
2015-10-22 08:45:58 -0500

    All in on 1.0.0.

--- agate/__init__.py
+++ agate/__init__.py
@@ -7,7 +7,7 @@ from agate.exceptions import *
 from agate.mapped_sequence import MappedSequence
 from agate.table import Table, allow_tableset_proxy
 from agate.tableset import TableSet
-from agate.warns import NullCalculationWarning
+from agate.warns import NullCalculationWarning, warn_null_calculation
 
 def save():
     raise NotImplementedError
