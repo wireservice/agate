919c34f0c3e9f384e91544b8e56c46968fcaf292
Christopher Groskopf <staringmonkey@gmail.com>
2015-11-05 10:39:17 -0600

    Add missing docstring for Summary.

--- agate/aggregations.py
+++ agate/aggregations.py
@@ -49,12 +49,18 @@ class Aggregation(object): #pragma: no cover
 class Summary(Aggregation):
     """
     An aggregation that can apply an arbitrary function to a column.
+
+    :param column_name:
+        The column being summarized.
+    :param data_type:
+        The return type of this aggregation.
+    :param func:
+        A function which will be passed the column for processing.
     """
-    def __init__(self, column_name, data_type, func, cache_key=None):
+    def __init__(self, column_name, data_type, func):
         self._column_name = column_name
         self._data_type = data_type
         self._func = func
-        self._cache_key = cache_key
 
     def get_aggregate_data_type(self, table):
         return self._data_type
