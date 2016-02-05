a1fdd999b0df0aa74868244ced4f8f1cff3ba471
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-02-05 15:04:48 -0600

    Rev to 1.2.3 for dev.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name='agate',
-    version='1.2.2',
+    version='1.2.3',
     description='A data analysis library that is optimized for humans instead of machines.',
     long_description=open('README.rst').read(),
     author='Christopher Groskopf',
