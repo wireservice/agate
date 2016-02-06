e7abf648a99255dc074f3230957bc2121a75f65b
Neil Bedi <neilbedi@gmail.com>
2016-02-06 15:34:13 -0500

    Allow multiple columns to compare in homogenize. #407

--- tests/test_table.py
+++ tests/test_table.py
@@ -1625,9 +1625,10 @@ class TestHomogenize(AgateTestCase):
         self.column_names = ['one', 'two', 'three']
         self.column_types = [self.number_type, self.number_type, self.text_type]
 
-    def test_homogenize(self):
+    def test_homogenize_column_name(self):
         table = Table(self.rows, self.column_names, self.column_types)
-        homogenized = table.homogenize('one', range(3), [3, 'd'])
+        compare_values = [[0], [1], [2]]
+        homogenized = table.homogenize(['one'], compare_values, [3, 'd'])
         rows = (
             (0, 4, 'a'),
             (1, 3, 'b'),
@@ -1639,13 +1640,34 @@ class TestHomogenize(AgateTestCase):
         self.assertColumnTypes(homogenized, [Number, Number, Text])
         self.assertRows(homogenized, rows)
 
-    def test_homogenize_lambda(self):
+    def test_homogenize_multiple_columns(self):
+        table = Table(self.rows, self.column_names, self.column_types)
+
+        def column_two(count):
+            return [chr(ord('a') + c) for c in range(count)]
+
+        homogenized = table.homogenize(['one', 'three'], zip(range(3), column_two(3)), [5])
+        rows = (
+            (0, 4, 'a'),
+            (1, 3, 'b'),
+            (None, 2, 'c'),
+            (2, 5, 'c')
+        )
+
+        self.assertColumnNames(homogenized, self.column_names)
+        self.assertColumnTypes(homogenized, [Number, Number, Text])
+        self.assertRows(homogenized, rows)
+
+    def test_homogenize_lambda_default(self):
         table = Table(self.rows, self.column_names, self.column_types)
 
         def default_row(d):
-            return [d * 2, chr(ord('a') + d)]
+            return [d[0], d[0] * 2, d[1]]
 
-        homogenized = table.homogenize('one', range(3), default_row)
+        def column_two(count):
+            return [chr(ord('a') + c) for c in range(count)]
+
+        homogenized = table.homogenize(['one', 'three'], zip(range(3), column_two(3)), default_row)
         rows = (
             (0, 4, 'a'),
             (1, 3, 'b'),
