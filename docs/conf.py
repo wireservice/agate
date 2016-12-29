4601a1bafdadd72b5f5d0f3fa405aeaac150b41a
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-12-29 15:23:49 -0600

    Rev to 1.5.6 for dev.

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,8 @@ master_doc = 'index'
 # Metadata
 project = u'agate'
 copyright = u'2016, Christopher Groskopf'
-version = '1.5.5'
-release = '1.5.5'
+version = '1.5.6'
+release = '1.5.6'
 
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
