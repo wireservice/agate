d8218f145763a04f2ef3f4a5f53b9c1d8fc10521
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-07-13 19:02:14 -0400

    build: Only test PyICU on Linux

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
             'cssselect>=0.9.1',
             'lxml>=3.6.0,<4.0.0',
             'nose>=1.1.2',
-            'PyICU>=2.4.2',
+            'PyICU>=2.4.2;sys_platform==linux',
             'pytz>=2015.4',
             'mock>=1.3.0;python_version<"3"',
             'unittest2>=1.1.0;python_version<"3"',
