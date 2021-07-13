c16ed62825a3b5ba9c538b899616cfce2fe18693
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-07-13 18:29:06 -0400

    build: Move requirements into extras_require

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
+from setuptools import find_packages, setup
 
-from setuptools import setup
-
-install_requires = [
-    'six>=1.9.0',
-    'pytimeparse>=1.1.5',
-    'parsedatetime>=2.1,!=2.5',
-    'Babel>=2.0',
-    'isodate>=0.5.4',
-    'python-slugify>=1.2.1',
-    'leather>=0.3.2',
-]
+with open('README.rst') as f:
+    long_description = f.read()
 
 setup(
     name='agate',
     version='1.6.2',
     description='A data analysis library that is optimized for humans instead of machines.',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     author='Christopher Groskopf',
     author_email='chrisgroskopf@gmail.com',
     url='http://agate.readthedocs.org/',
@@ -30,25 +22,39 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    packages=[
-        'agate',
-        'agate.aggregations',
-        'agate.computations',
-        'agate.data_types',
-        'agate.table',
-        'agate.tableset'
+    packages=find_packages(exclude=['benchmarks', 'tests', 'tests.*']),
+    install_requires=[
+        'Babel>=2.0',
+        'isodate>=0.5.4',
+        'leather>=0.3.2',
+        'parsedatetime>=2.1,!=2.5',
+        'python-slugify>=1.2.1',
+        'pytimeparse>=1.1.5',
+        'six>=1.9.0',
     ],
-    install_requires=install_requires
+    extras_require={
+        'test': [
+            'coverage>=3.7.1',
+            'cssselect>=0.9.1',
+            'lxml>=3.6.0,<4.0.0',
+            'nose>=1.1.2',
+            'PyICU>=2.4.2',
+            'pytz>=2015.4',
+            'mock>=1.3.0;python_version<"3"',
+            'unittest2>=1.1.0;python_version<"3"',
+        ],
+        'docs': [
+            'Sphinx>=1.2.2',
+            'sphinx_rtd_theme>=0.1.6',
+        ],
+    }
 )
