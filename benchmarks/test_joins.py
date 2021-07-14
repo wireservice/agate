abf824e153e330afec9ece4635d8767f4ef8fa9f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-07-14 00:18:03 -0400

    ci: Attempt Python 2.7 tests

--- benchmarks/test_joins.py
+++ benchmarks/test_joins.py
@@ -32,4 +32,4 @@ class TestTableJoin(unittest.TestCase):
 
         min_time = min(results)
 
-        self.assertLess(min_time, 10)  # CI unreliable, 5 witnessed
+        self.assertLess(min_time, 10)  # CI unreliable, 5s witnessed
