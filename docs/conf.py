0aa330053652221bdfd085558b46578c14537d69
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-13 16:54:55 -0400

    docs: Update theme

--- docs/conf.py
+++ docs/conf.py
@@ -34,14 +34,7 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 
 # HTMl theming
-html_theme = 'default'
-
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'furo'
 
 html_static_path = ['_static']
 htmlhelp_basename = 'agatedoc'
